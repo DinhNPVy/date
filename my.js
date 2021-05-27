8ecf2dfcce5ae0156493f727897abea852573993
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-05-27 20:19:39 +0700

    upload

---
+++ my.js
@@ -0,0 +1,56 @@
+function MyDate()
+{
+    let month = document.getElementById('month').value;
+
+    if(!month)
+    {
+        alert("Vui lòng nhập tháng để kiểm tra!");
+    }else if(month == 1)
+    {
+        alert("Tháng 1 có 31 ngày");
+    }else if(month == 2)
+    {
+        alert("Tháng 2 có 28 ngày");
+    }
+    else if(month == 3)
+    {
+        alert("Tháng 3 có 31 ngày");
+    }
+    else if(month == 4)
+    {
+        alert("Tháng 4 có 30 ngày");
+    }
+    else if(month == 5)
+    {
+        alert("Tháng 5 có 31 ngày");
+    }
+    else if(month == 6)
+    {
+        alert("Tháng 6 có 30 ngày");
+    }
+    else if(month == 7)
+    {
+        alert("Tháng 7 có 31 ngày");
+    }
+    else if(month == 8)
+    {
+        alert("Tháng 8 có 31 ngày");
+    }
+    else if(month == 9)
+    {
+        alert("Tháng 9 có 30 ngày");
+    }
+    else if(month == 10)
+    {
+        alert("Tháng 10 có 31 ngày");
+    }
+    else if(month == 11)
+    {
+        alert("Tháng 11 có 30 ngày");
+    }
+    else if(month == 12)
+    {
+        alert("Tháng 12 có 31 ngày");
+    }
+    
+}
